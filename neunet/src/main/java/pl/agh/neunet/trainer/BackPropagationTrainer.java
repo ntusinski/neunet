46ca0296d08146584bd2493f7529576eff4471ce
Norbert Tusiński <norbert.tusinski@gmail.com>
2013-09-22 16:34:03 +0200

    Second step of BP

--- neunet/src/main/java/pl/agh/neunet/trainer/BackPropagationTrainer.java
+++ neunet/src/main/java/pl/agh/neunet/trainer/BackPropagationTrainer.java
@@ -16,13 +16,15 @@ public class BackPropagationTrainer {
             int epochLength = epochsLengths.get(epoch);
             for (int epochItNumber = 0; epochItNumber < epochLength; epochItNumber++) {
                 int caseNumber = Math.abs(random.nextInt()) % learningInputData.size();
-                setInputNeuronWeightsAsInTrainingData(learningInputData.get(caseNumber), layers.get(0).getNeurons());
+                setInputNeuronSignalsAsInTrainingData(learningInputData.get(caseNumber), layers.get(0).getNeurons());
                 calculateOutputSignalsForAllNeurons(layers);
+                setOutputNeuronSignalsAsInTrainingData(learningOutputData.get(caseNumber), layers.get(layers.size() - 1).getNeurons());
+                calculateInputSignalsForAllNeurons(layers);
             }
         }
     }
 
-    private void setInputNeuronWeightsAsInTrainingData(List<Double> currentCaseData, List<Neuron> inputNeurons) {
+    private void setInputNeuronSignalsAsInTrainingData(List<Double> currentCaseData, List<Neuron> inputNeurons) {
         for (int i = 0; i < inputNeurons.size(); i++) {
             inputNeurons.get(i).setOutputSignal(currentCaseData.get(i));
         }
@@ -32,6 +34,20 @@ public class BackPropagationTrainer {
         for (int i = 1; i < layers.size(); i++) {
             for (Neuron neuron : layers.get(i).getNeurons()) {
                 neuron.updateOutputSignal();
+            }
+        }
+    }
+
+    private void setOutputNeuronSignalsAsInTrainingData(List<Double> currentCaseData, List<Neuron> outputNeurons) {
+        for (int i = 0; i < outputNeurons.size(); i++) {
+            outputNeurons.get(i).setInputSignal(currentCaseData.get(i));
+        }
+    }
+
+    private void calculateInputSignalsForAllNeurons(List<NetworkLayer> layers) {
+        for (int i = layers.size() - 2; i >= 0; i--) {
+            for (Neuron neuron : layers.get(i).getNeurons()) {
+                neuron.updateInputSignal();
             }
         }
     }
